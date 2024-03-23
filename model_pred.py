190bff6e2746ab90c0eb95d7a1f9df1bdcb6a54c
sehanasd <sehandvp@gmail.com>
2024-03-23 23:14:23 +0530

    requirements

---
+++ model_pred.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from model import get_prediction_from_url, main
+
+
+app = Flask(__name__)
+
+@app.route('/model_prediction', methods=['POST'])
+def model_prediction():
+    data = request.get_json()
+    url = data['url']
+
+    if url.startswith("https://"):
+        url = url.split("https://", 1)[-1]
+    elif url.startswith("http://"):
+        url = url.split("http://", 1)[-1]
+
+    features = main(url)
+
+    prediction = get_prediction_from_url(url)
+
+    return jsonify({'prediction': prediction})
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port= 5001)
